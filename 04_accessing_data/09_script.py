79fbb0df7ccfe7ee0875f4cf6937f3450621863a
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2023-03-21 11:20:00 +0100

    convert from json to df and back

--- 04_accessing_data/09_script.py
+++ 04_accessing_data/09_script.py
@@ -27,8 +27,28 @@ data = [
 # set the index of the data frame to be a multi index consisting of the employee and invoice columns
 df = pd.json_normalize(data, "sales", "employee").set_index(["employee", "invoice"])
 
+# print the data frame
 print(df)
+
+# reset the index of the data frame moving moves the multi index back into columns
+df = df.reset_index()
+
+# group the sales data by employee and convert the invoice and total data for each employee into a dictionary
+# create a new data frame using the groupby object
+# reset the index which moves the multi index back into columns
+# rename the first column to sales
+# convert the data frame to a json formatted string with records orientation
+json_doc = (df.groupby(["employee"], as_index = True)
+            .apply(lambda x: x[["invoice", "total"]].to_dict("records"))
+            .reset_index()
+            .rename(columns = {0: "sales"})
+            .to_json(orient = "records"))
+
+# load the json formatted string into a Python object then re-serialize it into a formatted json string
+# print the json formatted string
+print(json.dumps(json.loads(json_doc), indent = 2))
 
 # notes
     # script that converts json data into a data frame
+    # after converting it to a data frame it is converted back to json
     # pandas comes with various reader methods each of which is designed to load data in a certain format
